fafbc0edd0a5fd7c2568128db4a6a13beb785efd
sz-Bruno <114595639+sz-Bruno@users.noreply.github.com>
2022-10-18 08:36:59 -0300

    Add files via upload

---
+++ EXPLORER-STAGE4-1/script.js
@@ -0,0 +1,33 @@
+ 
+let num1= Number(window.prompt('digite um número'))
+let num2= Number(window.prompt('digite outro número'))
+let soma= (` ${num1+num2}`)
+let sub= (` ${num1-num2}`)
+let mult= (` ${num1* num2}`)
+let div=( ` ${num1/num2}`)
+let res1= (` ${num1%num2}`)
+
+window.alert("A soma dos números é:" + soma)
+window.alert("A subtração dos dois números é:" + sub)
+window.alert("A multiplicação dos dois números é:"+ mult)
+window.alert("A divisão dos dois números é:"+ div)
+window.alert("O resto da divisão dos dois números é:" + res1)
+
+function ParImpar(soma){
+    if (soma%2==0 ){
+        window.alert(`A soma dos números é par`)
+    }else{
+        window.alert('A soma dos números é ímpar')
+    }
+}
+
+function Igual(num1, num2){
+  if (num1=== num2){
+    window.alert(` À propósito,os números são iguais!`)
+  }else{
+    window.alert(`À propósito, os números são diferentes!`)
+  }
+}
+
+ParImpar(soma)
+Igual(num1,num2)
